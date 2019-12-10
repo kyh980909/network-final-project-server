e3d99f2869a6f4d3c573828a98fd3042c2931ec8
김용호 <kyh980909@gmail.com>
2019-12-11 02:32:47 +0900

    login, reigster, list 구현

---
+++ network-final-project-server/network-final-project-server/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace network_final_project_server
+{
+    class Program
+    {
+        static Socket mainSock; // 서버 소켓
+        static Dictionary<string, string> userInfo;
+        static Dictionary<string, Socket> connectedClients; // 클라이언트 소켓저장
+        public static void StartListening()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.1.186"), 9000);
+            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            connectedClients = new Dictionary<string, Socket>();
+            userInfo = new Dictionary<string, string>();
+
+            try
+            {
+                mainSock.Bind(endPoint);
+                mainSock.Listen(10);
+                Console.WriteLine("Server Info: {0}", endPoint);
+                Console.WriteLine("Server Listening...");
+                while (true)
+                {
+                    mainSock.BeginAccept(AcceptCallback, null);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n계속하려면 엔터를 누르세요...");
+            Console.Read();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            Console.WriteLine("Accept");
+            Socket client = mainSock.EndAccept(ar);
+            mainSock.BeginAccept(AcceptCallback, null);
+
+            AsyncObject obj = new AsyncObject(4096);
+            obj.WorkingSocket = client;
+            client.BeginReceive(obj.Buffer, 0, 4096, 0, DataReceived, obj);
+        }
+
+        public static void DataReceived(IAsyncResult ar)
+        {
+            Console.WriteLine("Receive");
+            AsyncObject obj = (AsyncObject)ar.AsyncState;
+
+            try
+            {
+                int received = obj.WorkingSocket.EndReceive(ar);
+
+                if (received <= 0)
+                {
+                    obj.WorkingSocket.Disconnect(false);
+                    obj.WorkingSocket.Close();
+                    return;
+                }
+
+                string data = Encoding.UTF8.GetString(obj.Buffer).Trim('\0');
+
+                auth(data, obj);
+
+                //for (int i = connectedClients.Count - 1; i >= 0; i--)
+                //{
+                //    Socket socket = connectedClients[i];
+                //    if (socket != obj.WorkingSocket)
+                //    {
+                //        try
+                //        {
+                //            socket.Send(obj.Buffer);
+                //        }
+                //        catch
+                //        {
+                //            try { socket.Dispose(); } catch { }
+                //            connectedClients.RemoveAt(i);
+                //        }
+                //    }
+                //}
+                //obj.WorkingSocket.Send(obj.Buffer);
+                //obj.ClearBuffer();
+                //obj.WorkingSocket.BeginReceive(obj.Buffer, 0, 4096, 0, DataReceived, obj);
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("{0} : 클라이언트 연결 종료", DateTime.Now));
+            }
+        }
+
+        public static void auth(string data, AsyncObject obj)
+        {
+            Console.WriteLine("auth");
+            var Response = new JObject(); // 반환할 데이터를 담을 객체
+            var readJson = JObject.Parse(data); // 받은 json 데이터 파싱
+            string req = readJson["req"].ToString();
+            Console.WriteLine(string.Format("{0} : {1}", DateTime.Now, readJson));
+            switch (req)
+            {
+                case "login":
+                    if (userInfo.ContainsKey(readJson["id"].ToString())) { // 아이디 존재 여부 체크
+                        if(userInfo[readJson["id"].ToString()] == readJson["pw"].ToString()) { // 아이디와 비밀번호가 맞는지 체크
+                            connectedClients.Add(readJson["id"].ToString(), obj.WorkingSocket);   // 로그인 성공한 유저의 아이디와 소켓을 딕셔너리에 저장
+                            Response.Add("res", "true");
+                        } else {
+                            Response.Add("res", "false");
+                            Response.Add("result", "비밀번호가 틀렸습니다.");
+                        }
+                    }
+                    else {
+                        Response.Add("res", "false");
+                        Response.Add("result", "존재하지 않는 아이디 입니다.");
+                    }
+                    break;
+
+                case "register":
+                    if(!userInfo.ContainsKey(readJson["id"].ToString())) {  // 아이디 중복체크
+                        userInfo.Add(readJson["id"].ToString(), readJson["pw"].ToString());
+                        Response.Add("res", "true");
+                    } else {
+                        Response.Add("res", "false");
+                        Response.Add("result", "존재하는 아이디 입니다.");
+                    }
+                    break;
+                case "list":
+                    foreach (var client in connectedClients.Keys)
+                    {
+                        Console.WriteLine(client);
+                    }
+                    break;
+            }
+
+            byte[] responseData = Encoding.UTF8.GetBytes(Response.ToString());
+            Console.WriteLine(string.Format("{0} : {1}", DateTime.Now, Response));
+            obj.WorkingSocket.Send(responseData);
+            obj.ClearBuffer();
+            obj.WorkingSocket.BeginReceive(obj.Buffer, 0, 4096, 0, DataReceived, obj);
+        }
+
+        static void Main(string[] args)
+        {
+            StartListening();
+        }
+    }
+}
